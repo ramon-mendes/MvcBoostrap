ac58edecb433005d8d1ca40a294da22e566cf20d
Ramon <rfmendesbrazil@gmail.com>
2021-01-25 16:10:38 -0300

    first commit

---
+++ MvcBoostrap/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MvcBoostrap.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
