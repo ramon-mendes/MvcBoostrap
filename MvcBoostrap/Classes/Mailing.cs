10b102e89d811855596a42a92a91512b2aae32ac
Ramon <rfmendesbrazil@gmail.com>
2021-03-28 15:23:29 -0300

    gogo

---
+++ MvcBoostrap/Classes/Mailing.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace MvcBoostrap.Classes
+{
+	public class Mailing
+	{
+		public static void SendMail(string to, string from, string subject, string msg)
+		{
+			MailMessage mail = new MailMessage();
+			mail.From = new MailAddress(from);
+			mail.To.Add(to); // para
+			mail.Subject = subject; // assunto
+			mail.Body = msg;
+
+			using(var smtp = new SmtpClient("smtp.gmail.com"))
+			{
+				smtp.EnableSsl = true;
+				smtp.Port = 587;
+				smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+				smtp.UseDefaultCredentials = false;
+				smtp.Credentials = new NetworkCredential("", "");
+				smtp.Send(mail);
+			}
+		}
+	}
+}
---
+++ MvcBoostrap/Controllers/ContatoController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcBoostrap.Controllers
+{
+	public class ContatoController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		[HttpPost]
+		public IActionResult Index(string name, string email, string msg)
+		{
+			return View();
+		}
+	}
+}
